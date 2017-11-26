36548f5a6a93811597de05fcc656e564060993f3
ruffordpereira <ruffordpereira@gmail.com>
2017-11-26 22:44:14 +0530

    d

--- Web-Programming Lab/P7/7.pl
+++ Web-Programming Lab/P7/7.pl
@@ -5,7 +5,7 @@ print "Refresh: 1\n";
 print "Content-Type:text/html\n\n";
 print start_html(-title=>"Digital Clock");
 ($s,$m,$h)=localtime(time);
-print p("The Current System Time Is $h:$m:$s");
+print p("The Currnet System Time Is $h:$m:$s");
 print hr;
 print p("In Words: $h hours $m minutes $s seconds");
 print end_html();
